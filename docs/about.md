d1e7a6a89498bc587f28cc6fb02d74083a8e2617
jokorone <jokroneberg@gmail.com>
2022-02-03 15:06:57 +0100

    add markdown

--- docs/about.md
+++ docs/about.md
@@ -1,3 +1,8 @@
 # About this App
 
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorum velit optio ab quis mollitia perspiciatis error ad sint expedita incidunt vel fuga vitae, vero magnam maxime assumenda exercitationem ratione.
+
+```js
+const settings = useSettings();
+
+```
--- src/components/content/markdown-content.tsx
+++ src/components/content/markdown-content.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { useFetch } from '../../util/fetch';
 import { Node } from './../../lib';
 
+import Markdown from 'markdown-to-jsx';
+
 type MarkdownContentProps = {
   selected: Node,
 }
 export const MarkdownContent = React.memo((props: MarkdownContentProps) => {
   const { status, content } = useFetch('about');
 
-  return <div className="absolute p-2 m-2 h-max w-5/12 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200">
-    {props.selected.id}
-    {content}
+  return <div className="absolute p-2 max-w-lg m-0 h-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200">
+    <Markdown options={{ wrapper: 'article' }}>
+      {content}
+    </Markdown>
   </div>
 });
